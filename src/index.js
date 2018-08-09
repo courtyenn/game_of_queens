a801f23e0b323a076bde9bc7f3e6f0cd65a4a79f
Courtney Nguyen <courtneythuy@gmail.com>
2018-08-09 16:10:47 -0700

    Some refactor

--- src/index.js
+++ src/index.js
@@ -2,32 +2,20 @@ import { Observable, Subscription, of, fromEvent, from, empty, merge, timer, Sub
 import { multicast, flatMap, concat, map, mapTo, switchMap, tap, mergeMap, takeUntil, filter, finalize, scan, pluck, every, mergeAll, withLatestFrom, concatMap } from 'rxjs/operators';
 import axios from 'axios'
 
-let cardMap = {
-    SPADES: [],
-    HEARTS: [],
-    CLUBS: [],
-    DIAMONDS: []
-}
-let deck_id;
-
 const fetchDeck = async () => (await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle`)).data.deck_id
 
-const getCardValue = (card) => {
+const getCardValue = (...ab) => {
     const cardValueMap = {
         'ACE': 20,
         'KING': 19,
         'QUEEN': 18,
         'JACK': 17
     }
-    return cardValueMap[card]
+    return ab.map(c => cardValueMap[c] ? cardValueMap[c] : parseInt(c))
 }
 
 const sortCards = (a, b) => {
-    let x = parseInt(a)
-    let y = parseInt(b)
-    if(!x)x = getCardValue(a);
-    if(!y)y=getCardValue(b);
-
+    let [x,y] = getCardValue(a,b)
     if(x < y)return -1
     if(x === y)return 0
     if(x > y)return 1
@@ -50,25 +38,28 @@ const pollDeck = (deckId, interval = 1000) => {
     let timer$ = timer(0, interval).pipe(switchMap(_ => draw(deckId)), takeUntil(testSubj))
     timer$.subscribe(pollSubject)
     pollSubject.pipe(filter(isQueen)).pipe(scan((acc, _) => acc + 1, 0)).pipe(filter(val => val >= 4)).subscribe(x => {
-        testSubj.next()
-    }, err => console.error(err), complete => {
-        Object.keys(cardMap).forEach(suit => {
-            console.log(suit + ': [' + cardMap[suit].sort(sortCards).toString()+ ']')
-        })
+        setTimeout(() => testSubj.next(), 1) // allow time for other observer to get new update before cancelling
     })
     return pollSubject;
 }
 
-const startDrawing = (deckId) => {
-
-}
-
 export const start = async () => {
     const deckId = await fetchDeck()
+    let cardMap = {
+        SPADES: [],
+        HEARTS: [],
+        CLUBS: [],
+        DIAMONDS: []
+    }
 
     pollDeck(deckId).subscribe(card => {
         cardMap[card.suit].push(card.value)
+    }, err => console.error(err), () => {
+        Object.keys(cardMap).forEach(suit => {
+            console.log(suit + ': [' + cardMap[suit].sort(sortCards).toString()+ ']')
+        })
     })
 }
 
 start();
+// start();
